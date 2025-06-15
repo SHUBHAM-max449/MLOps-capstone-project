7acc1be1cdda751f96bb40f5c4a216635ec36bfc
Shubham <chitaguppeshubham007@gmail.com>
2025-06-15 17:29:32 +0200

    Project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='an End to end MLOps project',
+    author='Shubham CHitaguppe',
+    license='MIT',
+)
